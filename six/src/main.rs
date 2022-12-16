ed82fe4e90a19368fcf5085eed56f239385bcd99
Ron Forrester <ron.forrester@me.com>
2022-12-16 01:01:05 -0500

    cleanup messages

--- six/src/main.rs
+++ six/src/main.rs
@@ -2,8 +2,7 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-fn count_chars( seq : &str, tc : char ) -> u32 {
-
+fn count_chars(seq: &str, tc: char) -> u32 {
     // println!("checking: {}", seq );
     let mut n = 0;
     for c in seq.chars() {
@@ -15,10 +14,9 @@ fn count_chars( seq : &str, tc : char ) -> u32 {
     n
 }
 
-fn is_start_of_packet( seq : &str ) -> bool {
-
+fn is_start_of_packet(seq: &str) -> bool {
     for c in seq.chars() {
-        if count_chars( seq, c ) > 1 {
+        if count_chars(seq, c) > 1 {
             return false;
         }
     }
@@ -26,14 +24,12 @@ fn is_start_of_packet( seq : &str ) -> bool {
     true
 }
 
-fn find_start_of_packet( line : &str, pkt_size : usize ) -> usize {
+fn find_start_of_packet(line: &str, pkt_size: usize) -> usize {
+    let mut i: usize = 0;
 
-    let mut i : usize = 0;
-
-    while i+4 < line.len() {
-
-        if is_start_of_packet( &line[i..=i+pkt_size-1] ) {
-            return i+pkt_size;
+    while i + 4 < line.len() {
+        if is_start_of_packet(&line[i..=i + pkt_size - 1]) {
+            return i + pkt_size;
         }
 
         i += 1;
@@ -43,18 +39,17 @@ fn find_start_of_packet( line : &str, pkt_size : usize ) -> usize {
 }
 
 fn main() {
-
     // Read lines into a vector
     //
     if let Ok(lines) = read_lines("input.txt") {
         // Consumes the iterator, returns an (Optional) String
         for line in lines.flatten() {
             if !line.is_empty() {
-                let sop : usize = find_start_of_packet( &line, 4 );
-                println!("Start of packet: {}", sop );
+                let sop: usize = find_start_of_packet(&line, 4);
+                println!("Start of packet: {}", sop);
 
-                let som : usize = find_start_of_packet( &line, 14 );
-                println!("Start of packet: {}", som );
+                let som: usize = find_start_of_packet(&line, 14);
+                println!("Start of message: {}", som);
             }
         }
     }
